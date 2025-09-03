b1a95641e18f845333661b8bd2a1179de82d5ab1
Johnnyboi9907 <johnnytalexiou@icloud.com>
2025-09-03 13:24:36 -0400

    Analyzable interface complete

---
+++ src/Lab01_B/Analyzable.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lab01_B;
+
+/**
+ *
+ * @author John
+ */
+public interface Analyzable {
+
+    double getAverage();
+
+    double getHighest();
+
+    double getLowest();
+}
